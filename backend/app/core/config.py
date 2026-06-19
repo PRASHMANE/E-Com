9e83a4ec45daba8a27df3a34dee6e22ba7b40745
PRASHMANE <prashanthvprashanthv46@gmail.com>
2026-06-19 11:09:49 +0530

    setup database and users table

---
+++ backend/app/core/config.py
@@ -0,0 +1,25 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    PROJECT_NAME: str
+
+    POSTGRES_SERVER: str
+    POSTGRES_PORT: int
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+
+    SECRET_KEY: str
+    ALGORITHM: str
+
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
+    REFRESH_TOKEN_EXPIRE_DAYS: int
+
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        case_sensitive=True
+    )
+
+
+settings = Settings()
---
+++ backend/app/models/user.py
@@ -0,0 +1,38 @@
+from sqlalchemy import Boolean
+from sqlalchemy import String
+
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+
+from app.db.base import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(
+        primary_key=True,
+        index=True
+    )
+
+    email: Mapped[str] = mapped_column(
+        String,
+        unique=True,
+        index=True,
+        nullable=False
+    )
+
+    hashed_password: Mapped[str] = mapped_column(
+        String,
+        nullable=False
+    )
+
+    is_active: Mapped[bool] = mapped_column(
+        Boolean,
+        default=True
+    )
+
+    is_verified: Mapped[bool] = mapped_column(
+        Boolean,
+        default=False
+    )
